444993b437be15b75bf2e49ef10747bae71c7f95
Eugen1344 <eugen1344@gmail.com>
2021-10-30 13:53:44 +0300

    test

---
+++ Assets/Scripts/Test/TestMemoryLeak.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMemoryLeak : MonoBehaviour
+{
+	public GameObject CarPrefab;
+
+	private List<GameObject> _cars = new List<GameObject>();
+
+	private void Start()
+	{
+		StartCoroutine(CloneAndDestroyCars());
+	}
+
+	private IEnumerator CloneAndDestroyCars()
+	{
+		while (true)
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				GameObject car = Instantiate(CarPrefab);
+				_cars.Add(car);
+			}
+
+			yield return new WaitForSeconds(1);
+
+			foreach (GameObject car in _cars)
+			{
+				Destroy(car);
+			}
+
+			_cars.Clear();
+
+			yield return new WaitForSeconds(1);
+		}
+	}
+}
