a973818eaadabdce7ef4cc055306d03eb733812d
Eugen1344 <eugen1344@gmail.com>
2021-12-04 08:14:54 +0200

    -space filling curves +nuget for unity +mathnet

---
+++ Assets/Scripts/Evolution/Fitness.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public abstract class Fitness : IComparable<Fitness>
+{
+	//public float Get
+
+	public int CompareTo(Fitness other)
+	{
+		throw new NotImplementedException();
+	}
+}
